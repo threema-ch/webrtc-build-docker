7d8f124495b7bac335dff44fc76714e617ca8fdc
Danilo Bargen <danilo.bargen@threema.ch>
2016-12-22 13:49:08 +0100

    Fix path to libjingle_peerconnection_java.jar

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@ set -e
 docker build -t threema/webrtc-build:latest build/
 CONTAINER=$(docker create threema/webrtc-build:latest)
 mkdir -p out/arm out/x86
+
 docker cp $CONTAINER:/webrtc/src/out/arm/libjingle_peerconnection_so.so out/arm/
 docker cp $CONTAINER:/webrtc/src/out/x86/libjingle_peerconnection_so.so out/x86/
 docker cp $CONTAINER:/webrtc/src/out/arm/lib.java/webrtc/base/base_java.jar out/
-docker cp $CONTAINER:/webrtc/src/out/arm/lib.java/webrtc/api/libjingle_peerconnection_java.jar out/
+docker cp $CONTAINER:/webrtc/src/out/arm/lib.java/webrtc/sdk/android/libjingle_peerconnection_java.jar out/
 docker rm $CONTAINER
 echo "Done. You can find the generated files in the out/ directory."
